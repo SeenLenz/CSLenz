8141665b798754bfde447b5d93ac92efb13f1dfa
Barnabás Csizmadia <cb20040621@gmail.com>
2022-03-02 17:37:03 +0100

    Add project files.

---
+++ SCLenz.cpp
@@ -0,0 +1,23 @@
+#include <Windows.h>
+#include "CSLenz.h"
+#include <tchar.h>
+#include <WinUser.h>
+#include <iostream>
+using namespace std;
+
+HHOOK SetWindowsHookExA(int idHook, HOOKPROC lpfn, HINSTANCE hmod, DWORD dwThreadId) 
+{
+
+}
+
+LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam) 
+{
+
+}
+
+int main()
+{
+
+
+	return 0;
+}
---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <Windows.h>
+#include "CSLenz.h"
+#include <tchar.h>
+#include <WinUser.h>
+#include <iostream>
+using namespace std;
+
+//HHOOK SetWindowsHookExA(int idHook, HOOKPROC lpfn, HINSTANCE hmod, DWORD dwThreadId) 
+//{
+//
+//}
+//
+//LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam) 
+//{
+//
+//}
+
+int main() 
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
